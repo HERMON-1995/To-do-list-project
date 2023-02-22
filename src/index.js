16ebfb554a0572a613fb86f42f1655ab0c1d8c6b
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-22 11:56:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import './style.css';
 
 import { addList, displayList } from './modules/UI.js';
+import clearCompleted from './modules/Clear_all.js';
 import localGet from './modules/Store.js';
 
 const form = document.querySelector('.add-list');
 const todoTask = document.getElementById('input');
+const clearAll = document.getElementById('complete-btn');
 
 window.addEventListener('load', () => {
   displayList();
@@ -14,5 +16,11 @@ window.addEventListener('load', () => {
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   addList(todoTask.value, false, localGet().length + 1);
+  displayList();
+});
+
+clearAll.addEventListener('click', (e) => {
+  e.preventDefault();
+  clearCompleted();
   displayList();
 });
