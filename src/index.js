ac8c172c75cac52dde57461dae208b0bd3e7f8c5
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-25 16:51:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import localGet from './modules/Store.js';
 const form = document.querySelector('.add-list');
 const todoTask = document.getElementById('input');
 const clearAll = document.getElementById('complete-btn');
+const rotate = document.getElementById('rotate');
 
 window.addEventListener('load', () => {
   displayList();
@@ -17,6 +18,14 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   addList(todoTask.value, false, localGet().length + 1);
   displayList();
+});
+
+rotate.addEventListener('click', (e) => {
+  e.preventDefault();
+  rotate.style.animation = 'rotation 2s';
+  const array = [];
+  window.localStorage.setItem('listStorage', JSON.stringify(array));
+  window.location.reload();
 });
 
 clearAll.addEventListener('click', (e) => {
